dae8a1f4552267cb8650116b024058f229c3e781
Chienfop533 <chienfop533@gmail.com>
2023-08-31 17:52:12 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 dotenv.config()
+import cors from 'cors'
 import { authRouter } from './routes'
 import connect from './db/db'
 import checkToken from './middleware/authentication'
 
 const app = express()
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 8000
 
 app.use(checkToken)
 app.use(express.json())
+app.use(cors())
 
 app.use('/auth', authRouter)
 
